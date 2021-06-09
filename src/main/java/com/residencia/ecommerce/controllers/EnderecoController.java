b9876773f39b28c4964c42170d330aba57d1b407
IgorStumpf <igorluizstumpf@gmail.com>
2021-06-09 00:51:12 -0300

    Endereco (VO, Service e Controller - mudancas)

--- src/main/java/com/residencia/ecommerce/controllers/EnderecoController.java
+++ src/main/java/com/residencia/ecommerce/controllers/EnderecoController.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.residencia.ecommerce.entities.Endereco;
 import com.residencia.ecommerce.services.EnderecoService;
+import com.residencia.ecommerce.vo.CategoriaVO;
+import com.residencia.ecommerce.vo.EnderecoVO;
 
 @RestController
 @RequestMapping("/endereco")
@@ -26,15 +29,26 @@ public class EnderecoController {
 	public EnderecoService enderecoService;
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Endereco> findById(@PathVariable Integer id) {
+	public ResponseEntity<EnderecoVO> findById(@PathVariable Integer id) {
 		HttpHeaders headers = new HttpHeaders();
 		return new ResponseEntity<>(enderecoService.findById(id), headers, HttpStatus.OK);
 	}
 
 	@GetMapping("/list")
-	public ResponseEntity<List<Endereco>> findAll() throws Exception {
+	public ResponseEntity<List<EnderecoVO>> findAll(
+			@RequestParam(required = false) Integer pagina,
+			@RequestParam(required = false) Integer qtdRegistros) 
+					throws Exception {
+		
 		HttpHeaders headers = new HttpHeaders();
-		return new ResponseEntity<>(enderecoService.findAll(), headers, HttpStatus.OK);
+		return new ResponseEntity<>(enderecoService.findAll(pagina, 
+				qtdRegistros), headers, HttpStatus.OK);
+	}
+	
+	@GetMapping("/count")
+	public Long count() {
+		return enderecoService.count();
+
 	}
 
 	@PostMapping
--- src/main/java/com/residencia/ecommerce/services/EnderecoService.java
+++ src/main/java/com/residencia/ecommerce/services/EnderecoService.java
@@ -1,14 +1,19 @@
- package com.residencia.ecommerce.services;
+package com.residencia.ecommerce.services;
+
+import java.util.ArrayList;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.residencia.ecommerce.entities.Cliente;
 import com.residencia.ecommerce.entities.Endereco;
 import com.residencia.ecommerce.repositories.EnderecoRepository;
+import com.residencia.ecommerce.vo.ClienteVO;
+import com.residencia.ecommerce.vo.EnderecoVO;
 
 @Service
 public class EnderecoService {
@@ -16,22 +21,74 @@ public class EnderecoService {
 	@Autowired
 	public EnderecoRepository enderecoRepository;
 
-	public Endereco findById(Integer id) {
-		return enderecoRepository.findById(id).get();
+	public EnderecoVO findById(Integer id) {
+		Endereco endereco = enderecoRepository.findById(id).get();
+		return converteEntidadeParaVO(endereco);
 	}
 
-	public List<Endereco> findAll() {
-		return enderecoRepository.findAll();
+	public List<EnderecoVO> findAll(Integer pagina, Integer qtdRegistros) throws Exception{
+		Pageable page = null;
+		List<Endereco> listEndereco= null;
+		List<Endereco> listEnderecoComPaginacao = null;
+		List<EnderecoVO> listEnderecoVO = new ArrayList();
+		try {
+			if (null != pagina && null != qtdRegistros) {
+
+				page = PageRequest.of(pagina, qtdRegistros);
+				listEnderecoComPaginacao = enderecoRepository.findAll(page).getContent();
+
+				for (Endereco lEndereco : listEnderecoComPaginacao) {
+					listEnderecoVO.add(converteEntidadeParaVO(lEndereco));
+				}
+
+			} else {
+				listEndereco = enderecoRepository.findAll();
+
+				for (Endereco lEndereco : listEndereco) {
+					listEnderecoVO.add(converteEntidadeParaVO(lEndereco));
+				}
+
+			}
+		} catch (Exception e) {
+			throw new Exception("Não foi possível recuperar a lista de pedidos ::" + e.getMessage());
+		}
+		return listEnderecoVO;
 	}
 
 	public Endereco save(Endereco endereco) {
 		return enderecoRepository.save(endereco);
 	}
 
+	public Long count() {
+		return enderecoRepository.count();
+	}
+
 	public Endereco update(Endereco endereco, Integer id) {
 		endereco.setEnderecoId(id);
 		return enderecoRepository.save(endereco);
 	}
+	
+	private EnderecoVO converteEntidadeParaVO(Endereco endereco) {
+		EnderecoVO enderecoVO = new EnderecoVO();
+		List<ClienteVO> listClienteVO = new ArrayList<>();
+		
+		enderecoVO.setEnderecoId(endereco.getEnderecoId());
+		enderecoVO.setLogradouro(endereco.getRua());
+		enderecoVO.setNumero(endereco.getNumero());
+		enderecoVO.setComplemento(endereco.getComplemento());
+		enderecoVO.setBairro(endereco.getBairro());
+		enderecoVO.setLocalidade(endereco.getCidade());
+		enderecoVO.setUf(endereco.getEstado());
+		enderecoVO.setCep(endereco.getCep());
+		
+		for (Cliente lCliente : endereco.getListCliente()) {
+			
+		}
+		
+		return enderecoVO;
+	}
+	
+	//Fazer metodo de conversao de VO para Entidade -> falta entender o SAVE
 
 	public void delete(Integer id) {
 		enderecoRepository.deleteById(id);
